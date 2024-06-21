c6345672a6c3b7f8b385197004313473f5ecb116
ArcanumLibella <fiona.roux@hetic.net>
2024-06-21 22:13:15 +0200

    feat: Add Hero schema component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,21 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface GlobalHero extends Schema.Component {
+  collectionName: 'components_global_heroes';
+  info: {
+    displayName: 'Hero';
+    icon: 'house';
+  };
+  attributes: {
+    label: Attribute.String;
+    link: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'global.hero': GlobalHero;
+    }
+  }
 }
