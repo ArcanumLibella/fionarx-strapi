8c012b441e02a5f6cd0f3ad0732528df878d7aae
ArcanumLibella <fiona.roux@hetic.net>
2024-07-02 14:49:25 +0200

    feat: S3 integration for media uploads

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,33 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
   'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+}, },
+}, ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET_KEY"),
+        region: env("AWS_REGION", "eu-west-3"),
+        params: {
+          Bucket: env("AWS_BUCKET", "fionarx-strapi-images"),
+        },
+}, },
+}, });
